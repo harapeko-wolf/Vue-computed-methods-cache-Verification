ecbcd44b38c4eb034da696a7d2951ee1b8dbe222
harapeko-wolf <32303847+harapeko-wolf@users.noreply.github.com>
2019-02-08 14:55:30 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app"><img width="25%" src="./assets/logo.png" /> <Cash /></div>
+</template>
+
+<script>
+import Cash from "./components/Cash";
+
+export default {
+  name: "App",
+  components: {
+    Cash
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Cash.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <div>
+      <h2>Computed</h2>
+      <ol>
+        <li>{{ computedNumber }}</li>
+        <li>{{ computedNumber }}</li>
+        <li>{{ computedNumber }}</li>
+      </ol>
+    </div>
+    <div>
+      <h2>Methods</h2>
+      <ol>
+        <li>{{ methodsNumber() }}</li>
+        <li>{{ methodsNumber() }}</li>
+        <li>{{ methodsNumber() }}</li>
+      </ol>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Cash",
+  data() {
+    return {};
+  },
+  computed: {
+    computedNumber: function() {
+      return Math.random();
+    }
+  },
+  methods: {
+    methodsNumber: function() {
+      return Math.random();
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  // list-style: ;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
